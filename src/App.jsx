ede47ce6e625805b0af9505a9f0f402ea6511103
abdullahbaa <aaziz191231@bscse.uiu.ac.bd>
2026-02-06 11:48:29 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Header from './Components/Header/Header'
+import Blogs from './components/blogs/Blogs'
+
+function App() {
+
+
+  return (
+    <>
+      <Blogs></Blogs>
+      <Header></Header>
+      
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+      <h1 className='text-4xl font-bold'>Book Knowledge</h1>
+            
+        </div>
+    );
+};
+
+export default Header;
